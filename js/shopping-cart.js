ae49803f09b9d5a9fc7bd3e22d1ad2dc5deeb61f
Kate-Sagittarius <kate.zakhovaiko@gmail.com>
2019-04-02 02:27:02 +0300

    add delete button functionality on shopping cart page

--- js/shopping-cart.js
+++ js/shopping-cart.js
@@ -1,56 +1,62 @@
 var quantity1 = 1;
 var quantity2 = 1;
-var sum1 = 1200;
-var sum2 = 2000;
 var price1 = 1200;
 var price2 = 2000;
 
-function total() {
+render();
+
+function render() {
+  var sum1 = quantity1 * price1;
+  var sum2 = quantity2 * price2;
   var totalSum = sum1 + sum2;
+
+  if (quantity1 === 0) {
+    document.getElementById('item1').className = 'sc_deleted';
+  } else {
+    document.getElementById('qty1').value = quantity1;
+    document.getElementById('sum1').innerHTML = sum1;
+  }
+
+  if (quantity2 === 0) {
+    document.getElementById('item2').className = 'sc_deleted';
+  } else {
+    document.getElementById('qty2').value = quantity2;
+    document.getElementById('sum2').innerHTML = sum2;
+  }
+  
   document.getElementById('total').innerHTML = totalSum;
-}
-
-total();
-
-function render1() {
-  document.getElementById('qty1').value = quantity1;
-  document.getElementById('sum1').innerHTML = sum1;
-  total();
-}
-
-function render2() {
-  document.getElementById('qty2').value = quantity2;
-  document.getElementById('sum2').innerHTML = sum2;
-  total();
 }
 
 function subQ1() {
   if (quantity1 > 1) {
-  quantity1--;
-  sum1 = price1 * quantity1;
+    quantity1--;
   }
-  render1();
+  render();
 }
 
 function subQ2() {
   if (quantity2 > 1) {
     quantity2--;
-    sum2 = price2 * quantity2;
   }
-  render2();
+  render();
 }
 
 function addQ1() {
   quantity1++;
-  sum1 = price1 * quantity1;
-  render1();
+  render();
 }
 
 function addQ2() {
   quantity2++;
-  sum2 = price2 * quantity2;
-  render2();
+  render();
 }
 
-render1();
-render2();
+function delItem1() {
+  quantity1 = 0;
+  render();
+}
+
+function delItem2() {
+  quantity2 = 0;
+  render();
+}
